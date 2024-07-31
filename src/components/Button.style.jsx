d1ae1f51b4fe4a84c223fbee99f3ba322d2a7af1
denisfl1 <denisfl1@hotmail.com>
2024-07-31 15:12:10 -0300

    teste

--- src/components/Button.style.jsx
+++ src/components/Button.style.jsx
@@ -12,9 +12,8 @@ height:${theme.sizes.xxl};
 
 &:hover{
     background-color:blue;
-    color:white
+    color:white;
 }
-
 `
 
 export const Button_2 = styled(Button2)`
@@ -22,3 +21,6 @@ background-color:lightblue;
 width:200px;
 height:80px;
 `
+
+
+const teste = 'teste'
